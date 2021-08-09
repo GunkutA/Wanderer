195e5c5510237443493c5d473f99f1d5d150338c
GunkutA <gagabeyoglu@hotmail.com>
2021-08-09 21:02:11 +0300

    Vertical and horizontal movement added.

--- Assets/Codes/RubyController.cs
+++ Assets/Codes/RubyController.cs
@@ -7,14 +7,18 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      //  QualitySettings.vSyncCount = 0;
+      //  Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f*horizontalMovement*Time.deltaTime;
+        position.y = position.y + 3.0f * verticalMovement*Time.deltaTime;
         transform.position = position;
     }
 }
